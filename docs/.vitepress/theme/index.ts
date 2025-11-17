42b6a4bb85d7b9635a0582df5633d0a0f7f927ec
Null <7566349+zhongjiayao@user.noreply.gitee.com>
2025-11-17 16:37:44 +0800

    refactor(docs): 优化ant-design-vue组件导入和注册方式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,8 +3,7 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { AntDesignContainer } from '@vitepress-demo-preview/component'
-// import { Button, Space, Input, Card, Alert, Tag, Descriptions,  List, Avatar, Spin, Table, Pagination } from 'ant-design-vue';
-import Antd from 'ant-design-vue';
+import { Button, Space, Input, Card, Alert, Tag, Descriptions,  List, Avatar, Spin, Table, Pagination } from 'ant-design-vue';
 import '@vitepress-demo-preview/component/dist/style.css'
 import './style.css'
 import 'ant-design-vue/dist/reset.css';
@@ -20,7 +19,6 @@ export default {
   enhanceApp({ app }) {
     // ...
     app.component('demo-preview', AntDesignContainer)
-    // app.use(Button).use(Space).use(Input).use(Card).use(Alert).use(Tag).use(Descriptions).use(List).use(Avatar).use(Spin).use(Table).use(Pagination)
-    app.use(Antd) 
+    app.use(Button).use(Space).use(Input).use(Card).use(Alert).use(Tag).use(Descriptions).use(List).use(Avatar).use(Spin).use(Table).use(Pagination)
   } 
 } satisfies Theme
